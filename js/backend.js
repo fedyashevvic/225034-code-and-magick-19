b3392987964c08e93cd3109290ac609e00cbdfdc
fedyashevvic <fedyashevvic@gmail.com>
2020-02-15 15:48:20 +0300

    Сделано 6-1

--- js/backend.js
+++ js/backend.js
@@ -1,31 +1,93 @@
 'use strict';
 
 (function () {
+  var SUCCESS_CODE = 200;
   var WIZARDS_URL = 'https://js.dump.academy/code-and-magick/data';
-  var load = function (onLoad, onError) {
+  var POST_URL = 'https://js.dump.academy/code-and-magick';
+  var TIMEOUT_LIMIT = 10000;
+  var wizardForm = document.querySelector('.setup-wizard-form');
+
+
+  var save = function (data, onLoad, onError) {
     var xhr = new XMLHttpRequest;
-    xhr.responceType = 'JSON';
+    xhr.responseType = 'json';
 
     xhr.addEventListener('load', function () {
-      if (xhr.status === 200) {
+      if (xhr.status === SUCCESS_CODE) {
         onLoad(xhr.response);
       } else {
-        onError(xhr.status + ' ' + xhr.statusText);
+        onError('Сервер недоступен, попробуйте позднее!');
       }
     });
+
+    xhr.addEventListener('error', function () {
+      onError('Ошибка сети, попробуйте позже!');
+    });
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос занял слишком много времени, проверьте скорость Вашего соединения!');
+    });
+
+    xhr.timeout = TIMEOUT_LIMIT;
+    
+    xhr.open('POST', POST_URL);
+    xhr.send(data);
+  };
+
+  var saveSuccessHandler = function () {
+    window.setup.setupWindow.classList.add('hidden');
+  };
+  var saveErrorHandler = function (data) {
+    var errorMsg = document.createElement('p');
+    errorMsg.textContent = data;
+    errorMsg.style.background = '#ff0000';
+    errorMsg.style.color = '#fff';
+    errorMsg.style.textAlign = 'center';
+    document.querySelector('button.setup-submit').after(errorMsg);
+  };
+
+  wizardForm.addEventListener('submit', function (evt) {
+    save(new FormData(wizardForm), saveSuccessHandler, saveErrorHandler);
+    evt.preventDefault();
+  });
+
+  var load = function (onLoad, onError) {
+    var xhr = new XMLHttpRequest;
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === SUCCESS_CODE) {
+        onLoad(xhr.response);
+      } else {
+        onError('К сожалению, похожих Волшебников не нашлось');
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      onError('К сожалению, произошел сбой сети, проверьте подключение к интернету!');
+    });
+
+    xhr.addEventListener('timeout', function () {
+      onError('К сожалению, запрос не успел выполнится, обновите страницу.');
+    });
+    xhr.timeout = TIMEOUT_LIMIT;
 
     xhr.open('GET', WIZARDS_URL);
     xhr.send();
   };
 
-  var loadHandler = function(data) {
-    console.log(data);
+  var loadSuccessHandler = function (data) {
+    window.setup.renderWizardItem(data);
   };
-  var errorHandler = function(data) {
-    console.log(data);
+  var loadErrorHandler = function (data) {
+    var errorMsg = document.createElement('p');
+    errorMsg.textContent = data;
+    errorMsg.style.background = "#ff0000";
+    errorMsg.style.color = "#fff";
+    document.querySelector('.setup-similar-list').appendChild(errorMsg);
+    document.querySelector('.setup-similar').classList.remove('hidden');
   };
 
-  load(loadHandler, errorHandler);
+  load(loadSuccessHandler, loadErrorHandler);
 
 
   window.backend = {
--- js/setup.js
+++ js/setup.js
@@ -1,45 +1,32 @@
 'use strict';
 
 (function () {
-  var WIZARD_NAMES = ['Иван', 'Хуан', 'Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-  var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
   var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
   var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
   var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
   var similarBlock = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
-  var fragment = document.createDocumentFragment();
   var numberOfWizards = 4;
-  var wizardsData = [];
   var setupWindow = document.querySelector('.setup');
   var coatSetup = document.querySelector('.setup-wizard .wizard-coat');
   var eyesSetup = document.querySelector('.setup-wizard .wizard-eyes');
   var fireballSetup = document.querySelector('.setup-fireball-wrap');
 
-
   var getRamdomData = function (arr) {
     var currNum = Math.round(Math.random() * (arr.length - 1));
     return arr[currNum];
   };
-  var generateWizardsData = function () {
-    for (var i = 0; i < numberOfWizards; i++) {
-      var currentData = {
-        name: getRamdomData(WIZARD_NAMES) + ' ' + getRamdomData(WIZARD_SURNAMES),
-        coatColor: getRamdomData(COAT_COLORS),
-        eyesColor: getRamdomData(EYES_COLORS)
-      };
-      wizardsData.push(currentData);
-    }
-  };
   var renderWizard = function (wizard) {
     var wizardElement = similarBlock.cloneNode(true);
     wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
-    wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
-    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
-    fragment.appendChild(wizardElement);
+    wizardElement.querySelector('.wizard-coat').style.fill = wizard.colorCoat;
+    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.colorEyes;
+    return wizardElement;
   };
   var renderWizardItem = function (wizard) {
+    var fragment = document.createDocumentFragment();
+
     for (var i = 0; i < numberOfWizards; i++) {
-      renderWizard(wizard[i]);
+      fragment.appendChild(renderWizard(wizard[i]));
     }
     document.querySelector('.setup-similar-list').appendChild(fragment);
     document.querySelector('.setup-similar').classList.remove('hidden');
@@ -65,15 +52,12 @@
   eyesSetup.addEventListener('click', colorWizardEyes);
   fireballSetup.addEventListener('click', colorWizardFireball);
 
-
-  generateWizardsData();
-  renderWizardItem(wizardsData);
-
   window.setup = {
     COAT_COLORS: COAT_COLORS,
     EYES_COLORS: EYES_COLORS,
     FIREBALL_COLORS: FIREBALL_COLORS,
     getRamdomData: getRamdomData,
+    renderWizardItem: renderWizardItem,
     setupWindow: setupWindow
   };
 })();
